de964fe22318a4072b7cb47741d1a01570c1f139
AlvaroFernandezRodriguez4 <carmageddon100@gmail.com>
2024-12-12 20:46:33 +0100

    Incluimos el scroll en el homscreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,15 +1,31 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import { useMovies } from '../hooks/useMovies'
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import React from 'react';
+import { useMovies } from '../hooks/useMovies';
 import Slider from '../components/Slider';
 
 export default function HomeScreen() {
-    const { nowPlaying, loading } = useMovies();
-    return (
-        <View>
-            <Text>HomeScreen</Text>
-        </View>
-    )
+  const { nowPlaying, loading, loadMoreMovies } = useMovies();
+
+  return (
+    <View style={styles.container}>
+      {nowPlaying ? (
+        <Slider
+          movies={nowPlaying.movies}
+          height={300}
+          backgroundColor="#f5f5f5"
+          loadMore={loadMoreMovies}
+        />
+      ) : (
+        <ActivityIndicator size="large" color="#000" />
+      )}
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
